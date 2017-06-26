44726816b76bf389a604b2160bbbb4cc89ffa5c8
anchaljain123 <anchaljain14@gmail.com>
2017-06-26 18:09:18 +0530

    coreection in signup

--- src/Backend/api/user/user.controller.js
+++ src/Backend/api/user/user.controller.js
@@ -2,7 +2,7 @@ const userService = require('./user.service');
 
 exports.saveUser = (req,res,next)=>{
     userService.saveUser(req.body,res);
-    next();
+
 };
 
 exports.getUser = (req,res,next) => {
